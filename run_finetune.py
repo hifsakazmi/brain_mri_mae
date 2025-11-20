40d0a8a4b1d01b954b691edc56818280c46a6d95
hifsakazmi <hifsa.kazmi@gmail.com>
2025-11-20 20:17:07 +0500

    Fixed method signature

--- run_finetune.py
+++ run_finetune.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     # Fine-tune on dataset2 with pre-trained MAE
     finetune_classifier(
         config, 
-        dataset_name="dataset2", 
-        pretrained_path=config.MAE_ENCODER_SAVE_PATH
+        dataset_name=config.DATASET, 
+        use_drive_checkpoint=True
     )
