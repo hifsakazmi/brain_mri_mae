5c41113655bc8a26791a2090e7b44a91b9d4213e
hifsakazmi <hifsa.kazmi@gmail.com>
2025-11-22 22:47:51 +0500

    Changed transformation on in dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -75,15 +75,13 @@ def get_dataloader(dataset_name="dataset1", split="train", batch_size=None, num_
 
     # Enhanced transforms with augmentation for training
     if split == "train":
-        transform = transforms.Compose([
-            transforms.Resize((img_size, img_size)),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomRotation(10),
-            transforms.ColorJitter(brightness=0.2, contrast=0.2),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                               std=[0.229, 0.224, 0.225])
-        ])
+    transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.RandomHorizontalFlip(p=0.5),  # Only safe augmentation
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                           std=[0.229, 0.224, 0.225])
+    ])
     else:
         transform = transforms.Compose([
             transforms.Resize((img_size, img_size)),
